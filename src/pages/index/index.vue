c33d7c7f530362c014e11bcb092bff0e9d19699b
sunbaoxu <1194063234@qq.com>
2019-09-30 14:13:25 +0800

    首页’

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,100 @@
 <template>
-  <div >
-      我是首页
-  </div>
+  <section class="home-wrap">
+    <ul>
+      <li v-for="(m,i) in arr" :key="i" class="g-border">
+        <dl class="g-dis">
+          <dt>
+            <image class="g-back" :src="m.imgUrl" ></image>
+          </dt>
+          <dd class="g-col-fen">
+            <h6>{{m.title}}</h6>
+            <p>{{m.time}}</p>
+          </dd>
+        </dl>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
-      
+      arr:[
+        {
+          title:'动物园+海洋馆',
+          time:'10月1日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/hyg.jpg'
+        },
+        {
+          title:'颐和园+鸟巢+水立方',
+          time:'10月2日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/yhy.jpg'
+        },
+        {
+          title:'恭王府+北海公园+什刹海',
+          time:'10月3日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/gwf.jpg'
+        },
+        {
+          title:'天安门+故宫',
+          time:'10月4日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/tam.jpg'
+        },
+        {
+          title:'长城+十三陵',
+          time:'10月5日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/cc.jpg'
+        },
+        {
+          title:'大鸭梨+电影',
+          time:'10月6日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/dyl.jpg'
+        },
+        {
+          title:'坐飞机回家',
+          time:'10月7日',
+          imgUrl:'http://tsfile.labifenqi.com/staticFile/public/demo/wx/fj.jpg'
+        }
+      ]
     }
   },
   methods: {
-    
+    showToast () {
+    }
+  },
+  mounted (){
+
   }
 }
 </script>
-
-<style scoped>
-
+<style lang="scss" scoped>
+.home-wrap{
+  ul{
+    li{
+      height: 90px;
+      dl{
+        height: 100%;
+        dt{
+          width: 130px;
+          height: 100%;
+          overflow: hidden;
+          position: relative;
+        }
+        dd{
+          width: 0;
+          flex: 1;
+          padding-left: 15px;
+          padding-top:6px;
+          padding-bottom: 6px;
+          p{
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
+
